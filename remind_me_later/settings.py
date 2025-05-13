94848e786cd12eebf380ee032c3a62b0523f44a6
BIKASH KUMAR SHARMA <bikashsharma5151@gmail.com>
2025-05-13 19:15:09 +0530

    Update settings.py

--- remind_me_later/settings.py
+++ remind_me_later/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = "c"
+EMAIL_HOST_PASSWORD = ""
 # Application definition
 
 INSTALLED_APPS = [
